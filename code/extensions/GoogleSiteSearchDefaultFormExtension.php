a93816fefff8e7eaa099b79dffc002d51680e917
Will Rossiter <will.rossiter@gmail.com>
2015-06-29 07:50:18 +1200

    Allow button label to be configured

--- code/extensions/GoogleSiteSearchDefaultFormExtension.php
+++ code/extensions/GoogleSiteSearchDefaultFormExtension.php
@@ -5,6 +5,10 @@
  */
 class GoogleSiteSearchDefaultFormExtension extends DataExtension {
 	
+	private static $submit_button_label = 'Go';
+	
+	private static $input_label = 'Search';
+	
 	/**
 	 * Return a form which sends the user to the first results page. If you want
 	 * to customize this form, use your own extension and apply that to the
@@ -14,11 +18,14 @@ class GoogleSiteSearchDefaultFormExtension extends DataExtension {
 	 */
 	public function getGoogleSiteSearchForm() {
 		if($page = GoogleSiteSearchPage::get()->first()) {
+			$label = Config::inst()->get('GoogleSiteSearchDefaultFormExtension', 'submit_button_label');
+			$formLabel = Config::inst()->get('GoogleSiteSearchDefaultFormExtension', 'input_label');
+			
 			$form = new Form(
 				$this, 
 				'GoogleSiteSearchForm', 
 				new FieldList(new TextField('Search')),
-				new FieldList(new FormAction('doSearch'))
+				new FieldList(new FormAction('doSearch', $label))
 			);
 
 			$form->setFormMethod('GET');
